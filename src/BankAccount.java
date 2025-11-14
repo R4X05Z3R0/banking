e41b177b3de23cc7402b12734e8b2ef582e98ce2
R4X05Z3R0 <razeroeuw@gmail.com>
2025-11-14 15:24:53 +0100

    Created SavingsAccount.java, BankAccount.java and KidsAccount.java

---
+++ src/BankAccount.java
@@ -0,0 +1,23 @@
+public abstract class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance){
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        setBalance(getBalance() + amount);
+    }
+
+    public void withdraw(double amount){
+        setBalance(getBalance() - amount);
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    protected void setBalance(double balance){
+        this.balance = balance;
+    }
+}
---
+++ src/KidsAccount.java
@@ -0,0 +1,5 @@
+public class KidsAccount extends SavingsAccount{
+    public KidsAccount(double balance){
+        super(balance,0.01);
+    }
+}
