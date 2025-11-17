f5b8fb91e5cc728818a395d75b3966afd50e05c4
R4X05Z3R0 <razeroeuw@gmail.com>
2025-11-17 09:51:48 +0100

    Created Bank.java

---
+++ src/Bank.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Bank {
+    ArrayList<BankAccount> accounts;
+
+    public Bank(){
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(BankAccount account){
+        accounts.add(account);
+    }
+
+    public ArrayList getInterestBearingAccounts(){
+        ArrayList interestAccounts = new ArrayList<>();
+        for(BankAccount account : accounts){
+            if (account instanceof InterestBearing){
+                interestAccounts.add(account);
+            }
+        }
+        return interestAccounts;
+    }
+}
